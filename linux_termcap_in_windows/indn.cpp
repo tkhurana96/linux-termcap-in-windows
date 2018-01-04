724dce5ff1744786d89f85987ac987e7d9427610
tkhurana96 <tusharkhurana99@gmail.com>
2018-01-04 07:35:43 +0530

    Format code for indn.cpp

--- linux_termcap_in_windows/indn.cpp
+++ linux_termcap_in_windows/indn.cpp
@@ -27,22 +27,22 @@ auto actual_indn(const short rowsToScroll = 1) {
 	std::cout << "\x1b[" << rowsToScroll << "S";
 }
 
-//int main() {
-//
-//	char bogus;
-//	std::string s(" line 1\n line 2\n line 3\n line 4\n line 5\n line 6\n line 7\n line 8\n line 9\n line 10\n line 11\n line 12\n line 13\n line 14\n line 15\n line 16\n line 17\n line 18\n line 19\n line 20\n line 21\n line 22\n line 23\n line 24\n line 25\n line 26\n line 27\n line 28\n line 29\n line 30\n line 31\n line 32\n line 33\n line 34\n line 35\n line 36\n line 37");
-//
-//	auto consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-//	CONSOLE_SCREEN_BUFFER_INFO buffInfo;
-//	SetConsoleTextAttribute(consoleHandle, FOREGROUND_GREEN | FOREGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_INTENSITY);
-//
-//	std::cout << s;
-//	GetConsoleScreenBufferInfo(consoleHandle, &buffInfo);
-//	SetConsoleCursorPosition(consoleHandle, { buffInfo.dwCursorPosition.X ,buffInfo.dwCursorPosition.Y - 32 });
-//	
-//	emulate_indn(5);
-//	// actual_indn(5);
-//
-//	std::cin >> bogus;
-//	return 0;
-//}
+int main() {
+
+	char bogus;
+	std::string s(" line 1\n line 2\n line 3\n line 4\n line 5\n line 6\n line 7\n line 8\n line 9\n line 10\n line 11\n line 12\n line 13\n line 14\n line 15\n line 16\n line 17\n line 18\n line 19\n line 20\n line 21\n line 22\n line 23\n line 24\n line 25\n line 26\n line 27\n line 28\n line 29\n line 30\n line 31\n line 32\n line 33\n line 34\n line 35\n line 36\n line 37");
+
+	auto consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO buffInfo;
+	SetConsoleTextAttribute(consoleHandle, FOREGROUND_GREEN | FOREGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_INTENSITY);
+
+	std::cout << s;
+	GetConsoleScreenBufferInfo(consoleHandle, &buffInfo);
+	SetConsoleCursorPosition(consoleHandle, { buffInfo.dwCursorPosition.X ,buffInfo.dwCursorPosition.Y - 32 });
+	
+	emulate_indn(5);
+	// actual_indn(5);
+
+	std::cin >> bogus;
+	return 0;
+}
